8bb8ab5ca3d3c1026c80a1f0168bd6fb123ac528
shrey-dev-2023 <workwithshreyarts@gmail.com>
2024-03-29 20:15:41 +0530

    CORS_UPD

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://air-bnb-clone-nine-beta.vercel.app",
   })
 );
 mongoose.connect(process.env.MONGO_URL);
